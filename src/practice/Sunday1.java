705b1c62e0b1be3087b56db3faa36f0a6a8b4227
malzu <malzu@10.0.0.198>
2023-04-09 15:40:39 -0400

    Iraq War

--- src/practice/Sunday1.java
+++ src/practice/Sunday1.java
@@ -5,4 +5,12 @@ public class Sunday1 {
 	public void home(String firstName, String lastName) {
 		System.out.println(firstName + lastName);
 	}
+	
+	public void address() {
+		System.out.println("Woodbridge");
+	}
+	
+	public static void iraq() {
+		System.out.println("Iraq Liberation was on 4/9/2003");
+	}
 }
